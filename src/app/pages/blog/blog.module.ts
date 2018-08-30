69ccaa08c074649db813fb7dbdeb7a4e5b81f964
vinsofts-thangnv <thangnv@vinsofts.net>
2018-08-30 16:11:05 +0700

    fix blog

--- src/app/pages/blog/blog.module.ts
+++ src/app/pages/blog/blog.module.ts
@@ -3,6 +3,14 @@ import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
 import { SharedModule } from '../../shared/shared.module';
 import { BlogComponent } from './blog.component';
+import { ContentBlogComponent } from '../../blocks/content-blog/content-blog.component';
+import { CategoriesBlogComponent } from '../../blocks/content-blog/categories-blog/categories-blog.component';
+import { LatestPostBlogComponent } from '../../blocks/content-blog/latest-post-blog/latest-post-blog.component';
+import { ProductBlogComponent } from '../../blocks/content-blog/product-blog/product-blog.component';
+import { SearchBlogComponent } from '../../blocks/content-blog/search-blog/search-blog.component';
+import { TagsBlogComponent } from '../../blocks/content-blog/tags-blog/tags-blog.component';
+import { TweetsBlogComponent } from '../../blocks/content-blog/tweets-blog/tweets-blog.component';
+
 
 const routesConfig: Routes = [
   { path: '', component: BlogComponent }
@@ -15,7 +23,14 @@ const routesConfig: Routes = [
     SharedModule
   ],
   declarations: [
-    BlogComponent
+    BlogComponent,
+    ContentBlogComponent,
+    CategoriesBlogComponent,
+    LatestPostBlogComponent,
+    ProductBlogComponent,
+    SearchBlogComponent,
+    TagsBlogComponent,
+    TweetsBlogComponent
   ]
 })
 export class BlogModule { }
