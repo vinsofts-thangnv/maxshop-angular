4a5cfd9133ede150206b6d6367f7241f4b4eba87
hoangdat243 <hoangdatsvit@gmail.com>
2018-08-31 13:49:08 +0700

    add shop

--- src/app/pages/shop1/shop1.component.ts
+++ src/app/pages/shop1/shop1.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+declare const $: any;
 
 @Component({
   selector: 'app-shop1',
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Shop1Component implements OnInit {
 
-  constructor() { }
+  constructor() { 
+    $.getScript('../../../assets/js/functions.js');
+  }
 
   ngOnInit() {
   }
