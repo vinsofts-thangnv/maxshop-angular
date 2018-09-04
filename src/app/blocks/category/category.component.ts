0ea92e601a030dddac4232d47c478feaab08c41c
kien <kiendo5695@gmail.com>
2018-09-04 14:15:32 +0700

    update data categories

--- src/app/blocks/category/category.component.ts
+++ src/app/blocks/category/category.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CategoryService } from '../../services/category/category.service';
 
 @Component({
   selector: 'app-category',
@@ -6,10 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./category.component.css']
 })
 export class CategoryComponent implements OnInit {
-
-  constructor() { }
+  data_all: any;
+  n = 0;
+  constructor(private categoryService: CategoryService) { }
 
   ngOnInit() {
+    this.getAll();
   }
-
+  getAll() {
+    this.categoryService.getAllCategories()
+      .then(resJson => console.log(this.data_all = resJson))
+      .catch(err => console.log(err));
+  }
 }
